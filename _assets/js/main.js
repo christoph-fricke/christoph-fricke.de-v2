24fd5305c2d64bd9b03144de878a1166de49d74c
Christoph Fricke <christoph@frickeonline.de>
2017-05-27 18:32:46 +0200

    Added client side mail handling

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -11,6 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 }, false);
 
+document.querySelector(".contact__form").addEventListener("submit", function (event) {
+    contactHandler(event);
+}, false)
+
 window.addEventListener("resize", function () {
     if (window.innerWidth > 450) {
         window.addEventListener("scroll", setUserScrolled, false);
@@ -64,11 +68,48 @@ function getScrollTop() {
  */
 function getToken() {
     var request = new XMLHttpRequest();
-    request.open("GET", "_assets/php/getToken.php", true);
+    request.open("POST", "_assets/php/getToken.php", true);
     request.send();
     request.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             token = request.responseText;
         }
     };
+}
+
+function contactHandler(event) {
+    event.preventDefault();
+    var name = document.querySelector("#name").value;
+    var email = document.querySelector("#email").value;
+    var message = document.querySelector("#message").value;
+    var feedbackField = document.querySelector(".contact__feedback");
+
+    if (name.length < 1 || email.length < 1 || message.length < 1) {
+        feedbackField.innerHTML = "No empty inputs allowed!";
+        feedbackField.style.color = "rgba(255, 152, 0, 0.87)";
+    } else if (!validateName(name)) {
+        feedbackField.innerHTML = "Please enter a valid name!";
+        feedbackField.style.color = "rgba(255, 152, 0, 0.87)";
+    } else if (!validateEmail(email)) {
+        feedbackField.innerHTML = "Please enter a valid email!";
+        feedbackField.style.color = "rgba(255, 152, 0, 0.87)";
+    } else {
+        var request = new XMLHttpRequest();
+        request.open("POST", "_assets/php/sendMail.php", true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send("name=" + name + "&email=" + email + "&message=" + message + "&token=" + token);
+        request.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {}
+        };
+    }
+}
+
+function validateEmail(email) {
+    var pattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return pattern.test(email);
+}
+
+function validateName(text) {
+    var pattern = /^[A-Za-z ]{1,}$/;
+    return pattern.test(text);
 }
