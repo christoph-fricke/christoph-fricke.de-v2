1829a33379f22707544abe952fc2ea52dd7039aa
Christoph Fricke <christoph@frickeonline.de>
2017-06-30 19:11:43 +0200

    Added message for an empty database or error

--- _assets/js/blog.js
+++ _assets/js/blog.js
@@ -51,38 +51,47 @@ function getBlogs() {
  */
 function blogHandler(data) {
     var blogContainer = document.querySelector(".entries__container");
-    for (var x = 0; x < data.length; x++) {
-        //Creates needed elements
-        var card = document.createElement("div");
-        card.setAttribute("class", "entries__card card");
+    if (data.length === 0) {
+        var message = "No blogs have been puplished yet."
+        var errorMessage = document.createElement("h3");
+        errorMessage.setAttribute("class", "entries__errorMessage");
+        errorMessage.appendChild(document.createTextNode(message));
 
-        var title = document.createElement("h1");
-        title.setAttribute("class", "entries__title");
-        title.appendChild(document.createTextNode(data[x].title));
+        blogContainer.appendChild(errorMessage);
+    } else {
+        for (var x = 0; x < data.length; x++) {
+            //Creates needed elements
+            var card = document.createElement("div");
+            card.setAttribute("class", "entries__card card");
 
-        var date = document.createElement("p");
-        date.setAttribute("class", "entries__date");
-        date.appendChild(document.createTextNode(data[x].date));
+            var title = document.createElement("h2");
+            title.setAttribute("class", "entries__title");
+            title.appendChild(document.createTextNode(data[x].title));
 
-        var summary = document.createElement("p");
-        summary.setAttribute("class", "entries__summary");
-        summary.appendChild(document.createTextNode(data[x].summary));
+            var date = document.createElement("p");
+            date.setAttribute("class", "entries__date");
+            date.appendChild(document.createTextNode(data[x].date));
 
-        var buttonContainer = document.createElement("div");
-        buttonContainer.setAttribute("class", "entries__buttonContainer");
+            var summary = document.createElement("p");
+            summary.setAttribute("class", "entries__summary");
+            summary.appendChild(document.createTextNode(data[x].summary));
 
-        var button = document.createElement("a");
-        button.setAttribute("class", "button--flat");
-        button.setAttribute("href", "page.php?blog=" + data[x].id);
-        button.appendChild(document.createTextNode("Read more"));
+            var buttonContainer = document.createElement("div");
+            buttonContainer.setAttribute("class", "entries__buttonContainer");
 
-        //Build the DOM
-        card.appendChild(title);
-        card.appendChild(date);
-        card.appendChild(summary);
-        buttonContainer.appendChild(button);
-        card.appendChild(buttonContainer);
+            var button = document.createElement("a");
+            button.setAttribute("class", "button--flat");
+            button.setAttribute("href", "page.php?blog=" + data[x].id);
+            button.appendChild(document.createTextNode("Read more"));
 
-        blogContainer.appendChild(card);
+            //Build the DOM
+            card.appendChild(title);
+            card.appendChild(date);
+            card.appendChild(summary);
+            buttonContainer.appendChild(button);
+            card.appendChild(buttonContainer);
+
+            blogContainer.appendChild(card);
+        }
     }
 }
