7f4599b815012c9ddcd03a3c43ed3c2b7360a80d
Christoph Fricke <christoph@frickeonline.de>
2018-05-06 21:55:04 +0200

    Remove Google Analytics to avoid new data security laws in the eu

--- blog/page.php
+++ blog/page.php
@@ -69,16 +69,6 @@ function getPreviousBlog($pdo, $blogID)
     <meta name="author" content="Christoph Fricke" />
     <meta name="description" content="Blog page by of Christoph Fricke" />
 
-    <script>
-        window.ga = window.ga || function () {
-            (ga.q = ga.q || []).push(arguments)
-        };
-        ga.l = +new Date;
-        ga('create', 'UA-100417616-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-    <script async src='https://www.google-analytics.com/analytics.js'></script>
-
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans:400,500,700|Roboto:400,500,700" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     <link rel="stylesheet" href="../_assets/css/blogPage.css" />
